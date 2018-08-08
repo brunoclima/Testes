83184e1b5c311d9b544812dbfccb3d9ec183aab9
Bruno Ferreira <bruno.ferreira@climatempo.com.br>
2018-08-08 08:55:54 -0300

    File rename and stuff

--- PHP/dates.php
+++ PHP/dates.php
@@ -11,4 +11,16 @@ $end->add(new DateInterval('P1M6D'));
 $diff = $end->diff($start);
 echo "Difference: " . $diff->format('%m month, %d days (total: %a days)') . "\n";
 // Diferença: 1 mês, 6 dias (total: 37 dias)
+
+if($start < $end) {
+    echo "Start is before end!\n";
+}
+
+// mostra todas as quintas-feiras entre $start e $end
+$periodInterval = DateInterval::createFromDateString('first thursday');
+$periodIterator = new DatePeriod($start, $periodInterval, $end, DatePeriod::EXCLUDE_START_DATE);
+foreach($periodIterator as $date) {
+    //mostra cada data no período
+    echo $date->format('Y-m-d') . " ";
+}
 ?>
